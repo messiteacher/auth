7a948841e5ae3dc39bd24e30162f8a68e617ea08
ShinYounHo <suh2963@gmail.com>
2025-02-04 10:23:21 +0900

    post comment

--- src/main/java/com/example/auth/domain/post/post/entity/Post.java
+++ src/main/java/com/example/auth/domain/post/post/entity/Post.java
@@ -26,7 +26,7 @@ public class Post extends BaseTime {
     @Builder.Default
     private List<Comment> comments = new ArrayList<>();
 
-    public void addComment(Member author, String content) {
+    public Comment addComment(Member author, String content) {
 
         Comment comment = Comment
                 .builder()
@@ -36,6 +36,8 @@ public class Post extends BaseTime {
                 .build();
 
         comments.add(comment);
+
+        return comment;
     }
 
     public Comment getCommentById(long id) {
