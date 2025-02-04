a8f7aee2c424b901dc7e4c7fca3df483078ce3e8
ShinYounHo <suh2963@gmail.com>
2025-02-04 10:11:21 +0900

    get comment

--- src/main/java/com/example/auth/domain/post/post/entity/Post.java
+++ src/main/java/com/example/auth/domain/post/post/entity/Post.java
@@ -3,6 +3,7 @@ package com.example.auth.domain.post.post.entity;
 import com.example.auth.domain.member.member.entity.Member;
 import com.example.auth.domain.post.comment.entity.Comment;
 import com.example.auth.global.entity.BaseTime;
+import com.example.auth.global.exception.ServiceException;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -35,5 +36,15 @@ public class Post extends BaseTime {
                 .build();
 
         comments.add(comment);
+    }
+
+    public Comment getCommentById(long id) {
+
+        return comments.stream()
+                .filter(comment -> comment.getId() == id)
+                .findFirst()
+                .orElseThrow(
+                        () -> new ServiceException("404-1", "존재하지 않는 댓글입니다.")
+                );
     }
 }
