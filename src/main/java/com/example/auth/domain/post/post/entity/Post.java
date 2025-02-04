d5d4c9e1ddbf69a8c6fc6acb10211346eb30d269
ShinYounHo <suh2963@gmail.com>
2025-02-04 09:14:16 +0900

    comment domain add

--- src/main/java/com/example/auth/domain/post/post/entity/Post.java
+++ src/main/java/com/example/auth/domain/post/post/entity/Post.java
@@ -1,11 +1,12 @@
 package com.example.auth.domain.post.post.entity;
 
 import com.example.auth.domain.member.member.entity.Member;
+import com.example.auth.domain.post.comment.entity.Comment;
 import com.example.auth.global.entity.BaseTime;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -19,5 +20,6 @@ public class Post extends BaseTime {
     private Member author;
     private String title;
     private String content;
-
+    @OneToMany(mappedBy = "post", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, orphanRemoval = true)
+    private List<Comment> comments;
 }
