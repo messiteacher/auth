a15b739b22eb7e65d90bcaab395f5d282d93d092
ShinYounHo <suh2963@gmail.com>
2025-02-04 09:23:44 +0900

    comment uri

--- src/main/java/com/example/auth/domain/post/comment/controller/ApiV1CommentController.java
+++ src/main/java/com/example/auth/domain/post/comment/controller/ApiV1CommentController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/comments")
+@RequestMapping("/api/v1/posts/{postId}/comments")
 public class ApiV1CommentController {
 }
